47615e13e527421c89b0a6255278fe97eb1ee869
dc <dc@dcs-MacBook-Pro-2.local>
2026-01-06 20:48:04 -0800

    remove

--- convert_llama_embedding.py
+++
@@ -1,9 +0,0 @@
-from nemo.collections import llm
-
-if __name__ == '__main__':
-    llm.import_ckpt(
-        model=llm.LlamaEmbeddingModel(config=llm.Llama32EmbeddingConfig1B()),
-        source="hf://meta-llama/Llama-3.2-1B",
-    )
-
-
